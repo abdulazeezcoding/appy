c0ebe04dab5a8758969dd13319e4cfebac1d976b
abdulaziz <abdulazizmohammed5103@gmail.com>
2024-02-12 10:02:45 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var firstName = 'Abu';
+var lastName = 'Najaah';
+
+const pi = 3.14;
+
+let isReady = false;
+
+console.log(pi);
+
+let num1 = 19;
+let num2 = 37;
+
+let result = `${num1} + ${num2} = ${num1+num2}`;
+console.log(result);
